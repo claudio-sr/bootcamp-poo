c4cddb1334b574320b2645bbe59e5d26d8fa3dfb
claudio-sr <jclaudiosiqueira.js@gmail.com>
2022-10-27 11:31:06 -0300

    Classes mentaria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import br.com.claudio.desafio.dominio.Curso;
+import br.com.claudio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+
+        Mentoria mentoria = new Mentoria();
+
+        curso1.setTitulo("Java basic");
+        curso1.setDescricao("Curso basico de Java");
+        curso1.setCargaHoraria(88);
+
+        mentoria.setTitulo("Java POO");
+        mentoria.setDescricao("Tudo de POO");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria);
     }
 }
