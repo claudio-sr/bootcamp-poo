d08041fa34b55483584ef56e7291564c32c24e68
claudio-sr <jclaudiosiqueira.js@gmail.com>
2022-10-28 10:06:39 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.claudio.desafio.dominio.Bootcamp;
 import br.com.claudio.desafio.dominio.Curso;
+import br.com.claudio.desafio.dominio.Dev;
 import br.com.claudio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -18,7 +20,29 @@ public class Main {
         mentoria.setDescription("Tudo de POO");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("POO");
+        bootcamp.setDescricao("POO com Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJohn = new Dev();
+        devJohn.setNome("John");
+        devJohn.inscreverBootcamp(bootcamp);
+        System.out.println("Coteúdos matriculados" + devJohn.getConteudosInscritos());
+        devJohn.progredir();
+        System.out.println("Coteúdos concluidos" + devJohn.getConteudosConcluidos());
+        System.out.println("XP:" + devJohn.calcularTotalXp());
+
+        Dev devAnna = new Dev();
+        devAnna.setNome("Anna");
+        devAnna.inscreverBootcamp(bootcamp);
+        System.out.println("Coteúdos matriculados" + devAnna.getConteudosInscritos());
+        devAnna.progredir();
+        System.out.println("Coteúdos concluidos" + devAnna.getConteudosConcluidos());
+        System.out.println("XP:" + devAnna.calcularTotalXp());
+
+
+
     }
 }
